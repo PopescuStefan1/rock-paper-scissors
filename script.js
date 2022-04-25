f1befd158c4d48b4b2e0494f9c54859d11a1ce10
Stefyro <stefan.ro95@gmail.com>
2022-04-25 20:48:09 +0300

    Add function to play a single round

--- script.js
+++ script.js
@@ -2,9 +2,25 @@ function computerPlay() {
     const throwables = ['rock', 'paper', 'scissors']
 
     let randomChoice = Math.floor(Math.random() * 3)
-    return throwables[randomChoice];
+    return throwables[randomChoice]
 }
 
-function playRound(playerSelection,computerSelection){
+function playRound(playerSelection = '', computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
 
+    if (playerSelection === computerSelection) {
+        return `Draw! You both picked ${playerSelection}`
+    }
+
+    if (playerSelection === 'rock' && computerSelection === 'paper' 
+    || playerSelection === 'paper' && computerSelection === 'scissors' 
+    || playerSelection === 'scissors' && computerSelection === 'rock') {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+
+    return `You win! ${playerSelection} beats ${computerSelection}`
 }
+
+const playerSelection='rock'
+const computerSelection=computerPlay()
+console.log(playRound(playerSelection,computerSelection))

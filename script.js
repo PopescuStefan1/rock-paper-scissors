b592e941dbf07553fd9775cfe87bffcdf9482cd3
Stefyro <stefan.ro95@gmail.com>
2022-04-25 20:39:42 +0300

    lowercase computer choices

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function computerPlay() {
-    const throwables = ['Rock', 'Paper', 'Scissors']
+    const throwables = ['rock', 'paper', 'scissors']
 
     let randomChoice = Math.floor(Math.random() * 3)
     return throwables[randomChoice];
 }
 
+function playRound(playerSelection,computerSelection){
+
+}

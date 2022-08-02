fb769e21bfd825f553711c6b1973f7919bd43819
Stefan <stefan.ro95@gmail.com>
2022-08-02 18:07:22 +0300

    Add style.css, remove 5 round game, add buttons for choices

--- script.js
+++ script.js
@@ -22,41 +22,41 @@ function playRound(playerSelection = '', computerSelection) {
     return 1
 }
 
-function game() {
-    let playerScore = 0
-    let computerScore = 0
+// function game() {
+//     let playerScore = 0
+//     let computerScore = 0
 
-    for (let i = 0; i < 5; i++) {
-        let playerSelection
-        while (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
-            playerSelection = prompt('Choose Rock/Paper/Scissors', 'Rock')
+//     for (let i = 0; i < 5; i++) {
+//         let playerSelection
+//         while (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+//             playerSelection = prompt('Choose Rock/Paper/Scissors', 'Rock')
 
-            playerSelection = playerSelection.toLowerCase()
-        }
-        let computerSelection = computerPlay()
+//             playerSelection = playerSelection.toLowerCase()
+//         }
+//         let computerSelection = computerPlay()
 
-        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
-        computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+//         playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+//         computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
 
-        const result = playRound(playerSelection, computerSelection)
+//         const result = playRound(playerSelection, computerSelection)
 
-        if (result === 0) {
-            console.log(`Draw! You both picked ${playerSelection}`)
-            i--
-        } else if (result === -1) {
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
-            computerScore++
-        } else {
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`)
-            playerScore++
-        }
+//         if (result === 0) {
+//             console.log(`Draw! You both picked ${playerSelection}`)
+//             i--
+//         } else if (result === -1) {
+//             console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+//             computerScore++
+//         } else {
+//             console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+//             playerScore++
+//         }
 
-        console.log(`Score : You - ${playerScore} | Computer - ${computerScore}`)
-    }
+//         console.log(`Score : You - ${playerScore} | Computer - ${computerScore}`)
+//     }
 
-    if (playerScore > computerScore) {
-        console.log(`Congratulations!!! You won ${playerScore} to ${computerScore}`)
-    } else {
-        console.log(`Unlucky! You lost ${computerScore} to ${playerScore}`)
-    }
-}
+//     if (playerScore > computerScore) {
+//         console.log(`Congratulations!!! You won ${playerScore} to ${computerScore}`)
+//     } else {
+//         console.log(`Unlucky! You lost ${computerScore} to ${playerScore}`)
+//     }
+// }
